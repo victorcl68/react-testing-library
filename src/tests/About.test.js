bbb2a2683922df07fcc95cddbaf109e35d6ce123
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-27 15:45:48 -0300

    Adjusting the first part of the test to check if the heading match with the text in it

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,16 +5,18 @@ import About from '../components/About';
 
 describe('Testing About Component, Requirement 2', () => {
   test('', () => {
-    const { getByRole, getAllByText, getByAltText } = render(
+    const { getByRole, getAllByText, getByAltText, getByText } = render(
       <MemoryRouter>
         <About />
       </MemoryRouter>,
     );
     const heading = getByRole('heading', { level: 2 });
+    const headingText = getByText(/About Pokédex/);
     const firstParagraph = getAllByText(/Pokémons/)[0];
     const secondParagraph = getAllByText(/Pokémons/)[1];
     const imageAlt = getByAltText('Pokédex');
     expect(heading).toBeInTheDocument();
+    expect(heading).toBe(headingText);
     expect(imageAlt).toBeInTheDocument();
     expect(imageAlt).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
     expect(firstParagraph).toBeInTheDocument();
