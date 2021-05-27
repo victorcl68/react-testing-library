bbfcd9f4276ef24b6ada2c48f8fe9ca54e5a59f9
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-27 15:08:19 -0300

    testing if there is a heading h2 with the appropriate text content

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import About from '../components/About';
 
 describe('Testing About Component, Requirement 2', () => {
-  test('', () => {});
+  test('', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
 });
