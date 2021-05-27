ae1bf06275ab74a6029dc2a71c60434e73c0e2ab
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-27 15:50:23 -0300

    Changing the tests to its and dividing then into multiple it tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,22 +4,41 @@ import { MemoryRouter } from 'react-router-dom';
 import About from '../components/About';
 
 describe('Testing About Component, Requirement 2', () => {
-  test('', () => {
-    const { getByRole, getAllByText, getByAltText, getByText } = render(
+  it('', () => {
+    const { getByRole, getByText } = render(
       <MemoryRouter>
         <About />
       </MemoryRouter>,
     );
     const heading = getByRole('heading', { level: 2 });
     const headingText = getByText(/About Pokédex/);
-    const firstParagraph = getAllByText(/Pokémons/)[0];
-    const secondParagraph = getAllByText(/Pokémons/)[1];
-    const imageAlt = getByAltText('Pokédex');
+
     expect(heading).toBeInTheDocument();
     expect(heading).toBe(headingText);
-    expect(imageAlt).toBeInTheDocument();
-    expect(imageAlt).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+
+  it('', () => {
+    const { getAllByText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const firstParagraph = getAllByText(/Pokémons/)[0];
+    const secondParagraph = getAllByText(/Pokémons/)[1];
+
     expect(firstParagraph).toBeInTheDocument();
     expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('', () => {
+    const { getByAltText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const imageAlt = getByAltText('Pokédex');
+
+    expect(imageAlt).toBeInTheDocument();
+    expect(imageAlt).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
