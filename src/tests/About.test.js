de2c2cd497700239471efbae79349689146f5764
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-27 14:54:04 -0300

    Importing React and About Component

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import About from '../components/About';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render, fireEvent } from '@testing-library/react';
+// import renderWithRouter from '../renderWithRouter';
+
 describe('', () => {
   test('', () => {});
 });
