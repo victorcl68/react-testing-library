a34ecdb004e5d0c7a19e28e72e68494b61983a80
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-27 16:47:18 -0300

    testing no favorite pokemon found message

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -6,5 +6,13 @@ import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Testing FavoritePokemons Component, Third Requirement', () => {
   it('tests if there are no favorite pokemon, "No favorite pokemon found" is shown',
-    () => {});
+    () => {
+      const { getByText } = render(
+        <MemoryRouter>
+          <FavoritePokemons />
+        </MemoryRouter>,
+      );
+      const noPokemonMessage = getByText(/No favorite pokemon found/);
+      expect(noPokemonMessage).toBeInTheDocument();
+    });
 });
