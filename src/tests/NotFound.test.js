7baffde171a319d83b25b2adb58ef76865e4e0be
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-27 21:42:02 -0300

    testing if there is a heading level 2 with the right message

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testing NotFound Component, Fourth Requirement', () => {
+  it('tests if there is a heading level 2,whith a message: "Page requested not found"',
+    () => {
+      const { getByText } = render(
+        <MemoryRouter>
+          <NotFound />
+        </MemoryRouter>,
+      );
+
+      const notFoundMessage = getByText(/Page requested not found/);
+
+      expect(notFoundMessage).toBeInTheDocument();
+    });
+});
