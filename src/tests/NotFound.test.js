292359812e3b4b5c44a3b779f242638e01431107
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-29 01:41:21 -0300

    Refactoring NotFound.test, using "screen", improving tests titles and catching mostly element by role

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { NotFound } from '../components';
 
 describe('Testing NotFound Component, Fourth Requirement', () => {
-  it('tests if there is a heading level 2,whith a message: "Page requested not found"',
+  it('there is a heading level 2,whith a message: "Page requested not found"',
     () => {
-      const { getByText } = render(
+      render(
         <MemoryRouter>
           <NotFound />
         </MemoryRouter>,
       );
 
-      const notFoundMessage = getByText(/Page requested not found/);
+      expect(screen.getByRole('img', {
+        name: 'Crying emoji',
+      }))
+        .toBeInTheDocument();
 
-      expect(notFoundMessage).toBeInTheDocument();
+      expect(screen.getByText(/Page requested not found/)).toBeInTheDocument();
     });
 
-  it('tests if there is an image with the right source',
+  it('there is an image with the right source',
     () => {
-      const { getByAltText } = render(
+      render(
         <MemoryRouter>
           <NotFound />
         </MemoryRouter>,
       );
 
-      const imageAlt = getByAltText(
-        'Pikachu crying because the page requested was not found',
-      );
-
-      expect(imageAlt).toBeInTheDocument();
-      expect(imageAlt).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+      expect(screen.getByRole('img', {
+        name: 'Pikachu crying because the page requested was not found',
+      }))
+        .toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
     });
 });
