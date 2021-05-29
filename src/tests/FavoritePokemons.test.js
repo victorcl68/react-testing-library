5b27e70dbe8789a58aaf21e0113b2e2c4fc630d1
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-29 01:33:41 -0300

    Refactoring FavoritePokemons.test using "screen" and  improving tests titles

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Testing FavoritePokemons Component, Third Requirement', () => {
-  it('tests if there are no favorite pokemon, "No favorite pokemon found" is shown',
+  it('there are no favorite pokemon, "No favorite pokemon found" is shown',
     () => {
-      const { getByText } = render(
+      render(
         <MemoryRouter>
           <FavoritePokemons />
         </MemoryRouter>,
       );
-      const noFavoriteMessage = getByText(/No favorite pokemon found/);
 
-      expect(noFavoriteMessage).toBeInTheDocument();
+      expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
     });
 });
