7b932ded0ccb4ecc427695775094ec941b7684da
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-29 00:51:41 -0300

    Refactoring App.test changing fireEvent to userEvent, using "screen", improving tests titles and catching mostly element by role

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,85 +1,102 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Requirement 1, Testing App.js', () => {
-  it('renders a reading with the text `Pokédex`', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const heading = getByText(/Pokédex/i);
-    expect(heading).toBeInTheDocument();
-  });
-
-  it('shows the Pokédex when the route is `/`', () => {
-    const { getByText } = render(
-      <MemoryRouter initialEntries={ ['/'] }>
-        <App />
-      </MemoryRouter>,
-    );
-    expect(getByText('Encountered pokémons')).toBeInTheDocument();
-  });
-
-  it('tests if there are three links on the top of the application', () => {
-    const { getByText, getAllByRole } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const firstLink = getAllByRole('link')[0];
-    const secondLink = getAllByRole('link')[1];
-    const thirdLink = getAllByRole('link')[2];
-    const firstLinkText = getByText(/Home/);
-    const secondLinkText = getByText(/About/);
-    const thirdLinkText = getByText(/Favorite Pokémons/);
-    expect(firstLink).toBe(firstLinkText);
-    expect(secondLink).toBe(secondLinkText);
-    expect(thirdLink).toBe(thirdLinkText);
-    expect(firstLink
-      && secondLink
-      && thirdLink
-      && firstLinkText
-      && secondLinkText
-      && thirdLinkText)
-      .toBeInTheDocument();
-  });
-
-  it('tests if when the Home Link is clicked the user ir redirected to a / pathname',
+  it('renders a heading with the text `Pokédex`',
     () => {
-      const { getByText, history } = renderWithRouter(<App />);
-      const firstLinkText = getByText(/Home/);
-      fireEvent.click(firstLinkText);
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      expect(screen.getByRole('heading', {
+        level: 1,
+        name: 'Pokédex',
+      })).toBeInTheDocument();
+    });
+
+  it('shows the Pokédex when the route is `/`',
+    () => {
+      render(
+        <MemoryRouter initialEntries={ ['/'] }>
+          <App />
+        </MemoryRouter>,
+      );
+
+      expect(screen.getByRole('heading', {
+        level: 2,
+        name: 'Encountered pokémons',
+      })).toBeInTheDocument();
+    });
+
+  it('there are three links on the top of the application with the right text',
+    () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      expect(screen.getByRole('link', {
+        name: 'Home',
+      })).toBeInTheDocument();
+
+      expect(screen.getByRole('link', {
+        name: 'About',
+      })).toBeInTheDocument();
+
+      expect(screen.getByRole('link', {
+        name: 'Favorite Pokémons',
+      })).toBeInTheDocument();
+    });
+
+  it('when "Home" Link is clicked the user is redirected to / pathname',
+    () => {
+      const { history } = renderWithRouter(<App />);
       const { pathname } = history.location;
+
+      userEvent.click(screen.getByRole('link', {
+        name: 'Home',
+      }));
+
       expect(pathname).toBe('/');
     });
 
-  it('when the About Link is clicked the user ir redirected to a /about pathname',
+  it('when "About" Link is clicked the user is redirected to /about pathname',
     () => {
-      const { getByText, history } = renderWithRouter(<App />);
-      const secondLinkText = getByText(/About/);
-      fireEvent.click(secondLinkText);
+      const { history } = renderWithRouter(<App />);
       const { pathname } = history.location;
+
+      userEvent.click(screen.getByRole('link', {
+        name: 'About',
+      }));
+
       expect(pathname).toBe('/about');
     });
 
-  it('when the Favorites Link is clicked the user ir redirected to a /favorites pathname',
+  it('when "Favorite Pokémons" is clicked the user is redirected to /favorites pathname',
     () => {
-      const { getByText, history } = renderWithRouter(<App />);
-      const thirdLinkText = getByText(/Favorite Pokémons/);
-      fireEvent.click(thirdLinkText);
+      const { history } = renderWithRouter(<App />);
       const { pathname } = history.location;
+
+      userEvent.click(screen.getByRole('link', {
+        name: 'Favorite Pokémons',
+      }));
+
       expect(pathname).toBe('/favorites');
     });
 
-  it('when some not known adress is used the user ir redirected to NotFound.js',
+  it('when some not known adress is accessed the user is redirected to NotFound.js',
     () => {
-      const { getByText, history } = renderWithRouter(<App />);
+      const { history } = renderWithRouter(<App />);
+
       history.push('/qualquer-pagina/que-nao-exista/');
-      const noMatching = getByText(/Page requested not found/);
-      expect(noMatching).toBeInTheDocument();
+
+      expect(screen.getByText(/Page requested not found/)).toBeInTheDocument();
     });
 });
