76c364ef54b2ddfa2f847c95d2baae413c239b0e
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-29 01:21:13 -0300

    Refactoring About.test changingt, using "screen", improving tests titles and catching mostly element by role

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,47 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import About from '../components/About';
 
 describe('Testing About Component, Requirement 2', () => {
-  it('tests if there is a level 2 heading with the "About Pokédex" text in the document',
+  it('there is a level 2 heading with the "About Pokédex" text in the document',
     () => {
-      const { getByRole, getByText } = render(
+      render(
         <MemoryRouter>
           <About />
         </MemoryRouter>,
       );
-      const heading = getByRole('heading', { level: 2 });
-      const headingText = getByText(/About Pokédex/);
 
-      expect(heading).toBeInTheDocument();
-      expect(heading).toBe(headingText);
+      expect(screen.getByRole('heading', {
+        level: 2,
+        name: 'About Pokédex',
+      }))
+        .toBeInTheDocument();
     });
 
-  it('tests if the first and second paragraph are about Pokédex',
+  it('the first and second paragraph are about Pokédex',
     () => {
-      const { getAllByText } = render(
+      render(
         <MemoryRouter>
           <About />
         </MemoryRouter>,
       );
-      const firstParagraph = getAllByText(/Pokémons/)[0];
-      const secondParagraph = getAllByText(/Pokémons/)[1];
+      const firstParagraph = screen.getAllByText(/Pokémons/)[0];
+      const secondParagraph = screen.getAllByText(/Pokémons/)[1];
 
       expect(firstParagraph).toBeInTheDocument();
       expect(secondParagraph).toBeInTheDocument();
     });
 
-  it('tests if the image have the appropriate source',
+  it('the image have the appropriate source and Alt',
     () => {
-      const { getByAltText } = render(
+      render(
         <MemoryRouter>
           <About />
         </MemoryRouter>,
       );
-      const imageAlt = getByAltText('Pokédex');
 
-      expect(imageAlt).toBeInTheDocument();
-      expect(imageAlt).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+      expect(screen.getByRole('img', {
+        name: 'Pokédex',
+      }))
+        .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
     });
 });
