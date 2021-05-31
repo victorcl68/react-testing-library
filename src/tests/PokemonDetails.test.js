21b16273472563af7f18024d3c550c18231e5a6c
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-31 02:23:47 -0300

    Testing if detailed information for the selected Pokémon is shown on the screen

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { /* render , */ screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testing PokemonDetails Component, Seventh Requirement', () => {
+  const MoreDetails = 'More details';
   it('there is a More Details Link in the App Component', () => {
     renderWithRouter(<App />);
 
     const MoreDetailsLink = screen.getByRole('link', {
-      name: 'More details',
+      name: MoreDetails,
     });
 
     expect(MoreDetailsLink).toBeInTheDocument();
   });
 
-  it('the user is redirected to PokemonsDetails screen', () => {
+  it('the user is redirected to Pokemons Details screen', () => {
     const { history } = renderWithRouter(<App />);
     const MoreDetailsLink = screen.getByRole('link', {
-      name: 'More details',
+      name: MoreDetails,
     });
 
     expect(MoreDetailsLink).toBeInTheDocument();
@@ -29,5 +30,33 @@ describe('Testing PokemonDetails Component, Seventh Requirement', () => {
     const { pathname } = history.location;
 
     expect(pathname).toBe('/pokemons/25');
+  });
+
+  it('detailed information for the selected Pokémon is shown on the screen', () => {
+    const { history } = renderWithRouter(<App />);
+    const MoreDetailsLink = screen.getByRole('link', {
+      name: MoreDetails,
+    });
+
+    expect(MoreDetailsLink).toBeInTheDocument();
+
+    userEvent.click(MoreDetailsLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: 'Pikachu Details',
+    })).toBeInTheDocument();
+
+    expect(MoreDetailsLink).not.toBeInTheDocument();
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    }));
+
+    expect(screen.getByText(/This intelligent Pokémon/));
   });
 });
