42bbc003410296b0281a0319cd9fe6b2eba9f0ee
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-01 00:34:15 -0300

    Tests if it is rendering of the card with the information of a certain Pokémon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { /* render , */ screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testing Pokemon Component, Sixth Requirement', () => {
+  describe('Rendering of the card with the information of a certain Pokémon',
+    () => {
+      const MoreDetails = 'More details';
+      it('there is a More Details Link in the App Component', () => {
+        renderWithRouter(<App />);
+        const MoreDetailsLink = screen.getByRole('link', {
+          name: MoreDetails,
+        });
+
+        userEvent.click(MoreDetailsLink);
+
+        expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+        expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
+        expect(screen.getByTestId('pokemon-weight'))
+          .toHaveTextContent('Average weight: 6.0 kg');
+
+        expect(screen.getAllByRole('img', {
+          name: 'Pikachu sprite',
+        })[0]).toBeInTheDocument();
+      });
+    });
+});
